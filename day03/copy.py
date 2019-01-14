7bd7953201bcbd5baba8b9dcf2c0f79020a6d891
guiaiy <531589111@qq.com>
2019-01-14 17:40:32 +0800

    none

--- day03/copy.py
+++ day03/copy.py
@@ -1,7 +1,19 @@
 ###将/bin/ls复制到/tmp/list
-with open('/bin/ls','rb') as f1:
-    with open('/tmp/list','wb')as f2:
-        data=f1.read(4096)
-        while data:
-            f2.write(data)
-            data=f1.read(4096)
+import sys
+
+# with open('/bin/ls','rb') as f1:
+#     with open('/tmp/list','wb')as f2:
+#         data=f1.read(4096)
+#         while data:
+#             f2.write(data)
+#             data=f1.read(4096)
+
+def copy(src,dest):
+    with open(src, 'rb') as f1:
+        with open(dest, 'wb')as f2:
+            data = f1.read(4096)
+            while data:
+                f2.write(data)
+                data = f1.read(4096)
+
+copy (sys.argv[1],sys.argv[2])
---
+++ day03/star.py
@@ -0,0 +1,6 @@
+def pstar(n=30):
+   return ('*'* n)
+
+if __name__ == '__main__':
+    print(pstar())
+    print(pstar(50))
