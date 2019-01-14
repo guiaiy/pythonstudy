2298a8af63b82fa38b726559d660a7977f6aaf0b
Administrator <531589111@qq.com>
2019-01-14 11:29:44 +0800

    none

---
+++ day03/list.py
@@ -0,0 +1,11 @@
+print([10])
+
+print([10+5])
+
+print([10+5 for i in range(10)])
+
+print([10+i for i in range(10)])
+
+print([10+i for i in range(10) if i %2 == 0])
+
+print([192.168.1.i for i in range(1,255)])
