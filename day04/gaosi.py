bbb4bf038a7900133b2a59117934a1de416a99ad
guiaiy <531589111@qq.com>
2019-01-15 09:42:53 +0800

    none

---
+++ day04/gaosi.py
@@ -0,0 +1,4 @@
+result=0
+for i in range(1,101):
+    result+= i
+print(result)
---
+++ day04/ranpass.py
@@ -0,0 +1,11 @@
+import random
+
+
+box='1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+'
+
+randpass=''
+for i in range(8):
+    str=random.choice(box)
+    randpass += str
+
+print(randpass)
