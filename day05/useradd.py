98e3121250ea4aff5c27e4139fbbfcb8fb06081d
Administrator <531589111@qq.com>
2019-01-16 15:24:39 +0800

    none

--- day05/useradd.py
+++ day05/useradd.py
@@ -7,7 +7,7 @@ def useradd():
     while result != 0:
         username = input('请重新输入用户名\n')
         result = checkid.check_id(username)
-    password = input('请输入6位以上密码\n')
+    password = getpass.getpass('请输入6位以上密码\n')
     while len(password) < 6:
         password = getpass.getpass('密码长度不够，请重新输入\n')
     return (username,password)
