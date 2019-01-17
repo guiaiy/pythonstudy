be90d5255612e97712c0cc4b56799b116007efed
guiaiy <531589111@qq.com>
2019-01-17 11:46:57 +0800

    update

---
+++ day06/checkid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import string
+import keyword
+###合法返回0，空值返回1，keword返回2，首字符不合法返回3，其他字符不合法返回4,空值返回4###
+
+def check_id(id):
+    str = string.ascii_letters+'_'
+    dig = string.digits
+    if id == '':
+        print('invalid:  \033[31;1myou input nothing!\033[0m')
+        return 1
+    if id in keyword.kwlist:
+        print('invalid:  \033[31;1m%s is a keyword!\033[0m' % id)
+        return 2
+    if id[0] not in str:
+        print('invalid:  \033[31;1mThe first position must be a letter or "_"!\033[0m')
+        return 3
+    for pos,otherletter in enumerate(id[1:]):
+        if otherletter not in str+dig:
+            print('invalid:  \033[31;1mThe %sth letter "%s" is invalid!\033[0m' % (pos+2,otherletter))
+            return 4
+    return 0
+
+
+if __name__ == '__main__':
+    check_id('')
---
+++ day06/user.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import checkid
+import getpass
+
+def useradd():
+    print('用户注册')
+    username = input('请输入用户名\n')
+    check = usercheck(username)
+    result = checkid.check_id(username)
+    while check == 'false' or result != 0:
+        if check == 'false':
+            username = input('用户名已经存在，请重新输入用户名\n')
+            check = usercheck(username)
+            result = checkid.check_id(username)
+        if result != 0:
+            username = input('请重新输入用户名\n')
+            result = checkid.check_id(username)
+    password = getpass.getpass('请输入6位以上密码\n')
+    while len(password) < 6:
+        password = getpass.getpass('密码长度不够，请重新输入\n')
+    return {username:password}
+
+def infosave():
+    with open('/var/www/userinfosave','a') as f1:
+        userinfo = useradd()
+        for user in userinfo:
+            info = user+':'+userinfo[user]
+            f1.write(info+'\n')
+        print('注册成功')
+
+def usercheck(username):
+    with open('/var/www/userinfosave') as f1:
+        userinfo = f1.readlines()
+        for line in userinfo:
+            user = line.replace(line[line.index(':'):],'')
+            if username == user:
+                return 'false'
+        return 'true'
+
+def passwordcheck(username,password):
+    with open('/var/www/userinfosave') as f1:
+        lines = f1.readlines()
+        for line in lines:
+            user = line.replace(line[line.index(':'):],'')
+            userpass = line.replace(line[0:line.index(':')+1],'')
+            if user == username and userpass == password+'\n':
+                return 'true'
+        return 'false'
+
+def login():
+    print('用户登录')
+    username = input('请输入用户名\n')
+    password = getpass.getpass('请输入密码\n')
+    userchec = usercheck(username)
+    passchec = passwordcheck(username,password)
+    false = 0
+    while userchec == 'true' and false < 3:
+        username = input('用户名不存在，请重新输入\n')
+        userchec = usercheck(username)
+        password = getpass.getpass('请输入密码\n')
+        passchec = passwordcheck(username, password)
+        false += 1
+    while passchec == 'false' and false < 3:
+        print('密码错误，请重新输入')
+        password = getpass.getpass('请输入密码\n')
+        passchec = passwordcheck(username,password)
+        false += 1
+    if false >= 3:
+        print('失败次数过多，退出程序')
+        return 'false'
+    print('登录成功')
+    with open('/var/www/'+username+'longinfo','w') as f2:
+        f2.write('login\n')
+
+if __name__ == '__main__':
+    login()
